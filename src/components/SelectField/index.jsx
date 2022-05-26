f2d3291831edd7e7c779eca79291463ac0311f23
guto1657 <gustavomatos.lima01@gmail.com>
2022-05-26 09:34:33 -0300

    'test'

--- src/components/SelectField/index.jsx
+++ src/components/SelectField/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useToggle from '../../hooks/useToggle';
 import {
   SelectWrapper,
@@ -16,6 +16,7 @@ export const SelectField = ({ title = '', options = [] }) => {
   const [isSelectOpen, toggleSelect] = useToggle();
   const [selectOption, setSelectOption] = useState(null);
   const [appState, actions] = useAppContext();
+  const { selectFilter, darkMode } = appState;
 
   const onOptionClicked = (value) => {
     if (value === selectOption) {
@@ -31,12 +32,12 @@ export const SelectField = ({ title = '', options = [] }) => {
 
   return (
     <SelectWrapper>
-      <SelectHeader onClick={() => toggleSelect()} dark={appState.darkMode ? 1 : 0}>
-        <SelectName>{!selectOption ? title : selectOption}</SelectName>
+      <SelectHeader onClick={() => toggleSelect()} dark={darkMode ? 1 : 0}>
+        <SelectName>{!selectFilter ? title : selectFilter}</SelectName>
         <ArrowIcon />
       </SelectHeader>
       <SelectListWrapper open={isSelectOpen}>
-        <DropdownList dark={appState.darkMode ? 1 : 0}>
+        <DropdownList dark={darkMode ? 1 : 0}>
           {options.length &&
             options.map((option, index) => (
               <ListItem key={index} onClick={() => onOptionClicked(option.value)}>
